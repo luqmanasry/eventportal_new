a6532110bfd97bbbd3af0a21ef4554171f043775
luqmanasry <luqmanasry@gmail.com>
2025-07-03 19:43:03 +0800

    Fix

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Luqman\event_portal\eventhorizon-api\venv
+set VIRTUAL_ENV=C:\Users\Luqman\Downloads\eventhorizon-api\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
